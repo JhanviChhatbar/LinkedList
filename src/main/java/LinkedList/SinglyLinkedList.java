203347e96448c2f411a12495d999f9a8eb5ea5ca
Jhanvi Chhatbar <jhanvi.chhatbar31@gmail.com>
2025-06-07 23:20:44 +0530

    LinkedList

---
+++ src/main/java/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,62 @@
+package LinkedList;
+
+public class SinglyLinkedList {
+
+    Node head;
+
+    public void insertAtHead(int data){
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void insertAtTail(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node currentNode = head;
+        while (currentNode.next != null){
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
+
+    public void insertAtPosition(int data, int position){
+
+        if (position == 0){
+            insertAtHead(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+
+        int currentPosition = 1;
+        Node current = head;
+
+        while(currentPosition < position && current.next != null){
+            current = current.next;
+            currentPosition++;
+        }
+
+        if(currentPosition < position)
+            throw new  IndexOutOfBoundsException();
+
+        newNode.next = current.next;
+        current.next = newNode;
+
+    }
+
+    public void print(){
+        Node current = head;
+
+        while (current != null){
+            System.out.println(current.data + " -> ");
+            current = current.next;
+        }
+    }
+}
